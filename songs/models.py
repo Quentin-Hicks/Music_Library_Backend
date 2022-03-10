98c8bc160d885fbc878b5d0faa929335c15975c3
unknown <qwhick2@gmail.com>
2022-03-10 17:55:52 -0500

    Created title, artist, album, release_date, genre Property Names

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,11 @@
+from platform import release
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    title = models.CharField()
+    artist = models.CharField()
+    album = models.CharField()
+    release_date = models.DateField()
+    genre = models.CharField()
