f6e3331d4a85b430415a87128ddb4267022484db
unknown <qwhick2@gmail.com>
2022-03-11 11:36:13 -0500

    Created Song Serializer

---
+++ songs/serializers.py
@@ -0,0 +1,8 @@
+from dataclasses import field
+from rest_framework import serializers
+from .models import Song
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ['id', 'title', 'artist', 'release_date', 'genre']
