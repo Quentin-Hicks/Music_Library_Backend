9f6ec21d915ed0f57620f8ac03071b56151b026b
unknown <qwhick2@gmail.com>
2022-03-11 10:55:00 -0500

    Created Property Names In models.py File

--- songs/models.py
+++ songs/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 
 class Song(models.Model):
-    title = models.CharField()
-    artist = models.CharField()
-    album = models.CharField()
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
     release_date = models.DateField()
-    genre = models.CharField()
+    genre = models.CharField(max_length=255)
